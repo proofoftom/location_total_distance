98cf78f811e47f0650867dea958ca5621e54611c
Tommy Cox <tommy.cox@wavecloud.com>
2014-09-22 11:51:09 -0600

    Added files

---
+++ location_total_distance.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file The template file to render the calculated total distance
+ * Created by PhpStorm.
+ * User: Tommy Cox
+ * Date: 7/29/14
+ * Time: 6:28 AM
+ */
+?>
+
+<div class='location-total-distance'><b>Total Driving Distance:</b> <?php echo "$total_distance $units" ?></div>
